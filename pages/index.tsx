161361e0562f826df5ee571190ed4ad53b73445f
arsczx <arsendjangrachev@gmail.com>
2023-07-07 19:10:58 +0600

    made/sectioncomponents

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Section from "@/components/Section/Section";
 import Header from "../src/components/header/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Section/>
     </>
   );
 }
