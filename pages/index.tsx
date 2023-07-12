0f5f39b9bda1723e2ce47d5ce3fd524d23bd0ebc
BatyBoka <kitasosa18@gmail.com>
2023-07-12 23:11:34 +0600

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Section from "@/components/Section/Section";
 import Header from "../src/components/header/Header";
+import Vakansii from "@/components/vakansii/Vakansii";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Section/>
+      <Vakansii/>
     </>
   );
 }
