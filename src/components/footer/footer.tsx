25c3834ba1bbcc794635c38047cbc274b6e24322
Saadatov <saadatovbekmyrza59@gmail.com>
2023-07-18 20:42:39 +0600

    fix/footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,10 +6,12 @@ import google from "../../../public/images/footer/google.svg";
 import facebook from "../../../public/images/footer/facebook.svg";
 import inst from "../../../public/images/footer/inst.svg";
 import { useTranslation } from "react-i18next";
+import Link from "next/link";
+import { Router, useRouter } from "next/router";
 
 const Footer = () => {
   const chunkSize = [3, 3, 4];
-  const {t} = useTranslation()
+  const { t } = useTranslation();
 
   const chunks = useMemo(() => {
     let start = 0;
@@ -19,13 +21,23 @@ const Footer = () => {
       return chunk;
     });
   }, [footerData, chunkSize]);
+  const router = useRouter();
 
   const getData = useMemo(() => {
     return chunks.map((chunk, index) => (
+      // <div key={item.id} onClick={() => {
+      //   router.push(${item.link})
+      // }}>
+      //   <h6 className={scss.mainBtns}>{t(item.title)}</h6>
+      // </div>
       <div key={index} className={scss.content}>
         {index === 0 && <h2>compas</h2>}
         {chunk.map((item) => (
-          <h3 className={scss.txt} key={item.id}>{t(item.title)}</h3>
+          <h3 className={scss.txt} key={item.id}  onClick={() => {
+            router.push(`${item.link}`)
+          }}>
+            {t(item.title)}
+          </h3>
         ))}
       </div>
     ));
@@ -34,16 +46,14 @@ const Footer = () => {
   return (
     <div className={scss.wrapper}>
       <div className={scss.box_content}>
-        <div className={scss.box}>
-          {getData}
-        </div>
+        <div className={scss.box}>{getData}</div>
         <div className={scss.img}>
           <h4>compas</h4>
           <div className={scss.line2}></div>
           <div>
-          <Image src={google} alt="" />
-          <Image src={facebook} alt="" />
-          <Image src={inst} alt="" />
+            <Image src={google} alt="" />
+            <Image src={facebook} alt="" />
+            <Image src={inst} alt="" />
           </div>
         </div>
       </div>
--- src/constants/footer.ts
+++ src/constants/footer.ts
@@ -1,51 +1,54 @@
 interface IFooter {
     title:string,
     id: number,
+    link:string,
 }
 export const footerData:IFooter [] = [
     {
         title:"footer.aboutUs_title",
         id:1,
+        link:"/AboutPage",
+        
     },
-    {
-        title:"footer.news_services_title",
-        id:2,
-    },
-    {
-        title:"footer.help_title",
-        id:3,
-    },
-    {
-        title:"footer.employers_title",
-        id:4,
-    },
-    {
-        title:"footer.post_a_job_title",
-        id:5,
-    },
-    {
-        title:"footer.resumeSection_title",
-        id:6,
-    },
-    {
-        title:"footer.applicants_title",
-        id:7,
-    },
-    {
-        title:"footer.addResume_title",
-        id:8,
-    },
-    {
-        title:"footer.section_of_vacancies_title",
-        id:9,
-    },
-    {
-        title:"footer.companies_title",
-        id:10,
-    },
+//     {
+//         title:"footer.news_services_title",
+//         id:2,
+//     },
+//     {
+//         title:"footer.help_title",
+//         id:3,
+//     },
+//     {
+//         title:"footer.employers_title",
+//         id:4,
+//     },
+//     {
+//         title:"footer.post_a_job_title",
+//         id:5,
+//     },
+//     {
+//         title:"footer.resumeSection_title",
+//         id:6,
+//     },
+//     {
+//         title:"footer.applicants_title",
+//         id:7,
+//     },
+//     {
+//         title:"footer.addResume_title",
+//         id:8,
+//     },
+//     {
+//         title:"footer.section_of_vacancies_title",
+//         id:9,
+//     },
+//     {
+//         title:"footer.companies_title",
+//         id:10,
+//     },
     
-    {
-        title:"compas",
-        id:13
-    }
+//     {
+//         title:"compas",
+//         id:13
+//     }
 ]
