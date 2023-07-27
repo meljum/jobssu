00267b37d800886cd0424a3ab4a4b58869af169e
Almo <temirkanovalmanbet5@gmail.com>
2023-07-27 18:27:37 +0300

    global fix main component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,15 @@ import Main from "@/components/main/Main";
 import Section from "@/components/Section/Section";
 import Header from "@/components/header/Header";
 import Vakansii from "@/components/vakansii/Vakansii";
+import { MainTitle } from "@/constants/Main";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Main/>
-      <Section/>
-      <Vakansii/>
+      <Main array={MainTitle} />
+      <Section />
+      <Vakansii />
     </>
   );
 }
---
+++ src/constants/Main.ts
@@ -0,0 +1,24 @@
+interface IDataMain {
+  id: number;
+  quantity: string;
+  title: string;
+}
+
+export const MainDataArr: IDataMain[] = [
+  {
+    id: 1,
+    quantity: "860 +",
+    title: "main.main__jobs_title",
+  },
+  {
+    id: 2,
+    quantity: "29 345 +",
+    title: "main.main__summary_title",
+  },
+  {
+    id: 3,
+    quantity: "1 987 +",
+    title: "main.main__companies_title",
+  },
+];
+export const MainTitle = [{ id: 1, title: "main.main__title" }];
