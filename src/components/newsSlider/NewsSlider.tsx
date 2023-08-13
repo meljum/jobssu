52bde5f8133303e19bae85c5f3fc61a21c29e3ff
Ammarz83~ <sapitovammar@gmail.com>
2023-08-13 14:09:28 +0600

    commit slider

---
+++ src/components/newsSlider/NewsSlider.tsx
@@ -0,0 +1,61 @@
+import React, { useMemo } from 'react'
+import Slider from 'react-slick';
+import css from './NewsSlider.module.scss'
+import { useTranslation } from 'react-i18next';
+import { SliderData } from '@/constants/Slider';
+import Image from 'next/image';
+import pic from '../../../public/images/pic2.svg'
+import prev from '../../../public/images/Vector.svg'
+import next from '../../../public/images/Vector (1).svg'
+import nexts from '../../../public/images/next.png'
+import prevs from '../../../public/images/download.png'
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const NewsSlider: React.FC = () => {
+  const { t } = useTranslation();
+  const sliderSettings = {    
+    slickNext: true,
+    slickPrev: true,
+    Infinite:true,
+    speed: 500,
+    slidesToShow: 2.24,
+    slidesToScroll: 1,
+    prevArrow: <button className="slider-arrow prev-arrow"><img className={css.arr1} src={prev} alt="Previous" /></button>,
+    nextArrow: <button className="slider-arrow next-arrow"><img className={css.arr2} src={next} alt="Next" /></button>
+  }
+  const renderSlider = useMemo(() => {
+    return SliderData.map((e) => (
+      <div className={css.wrapper}>
+        <div className={css.right}>
+
+          <div key={e.id} className={css.card}>
+            <Image src={pic} width={286} height={164} alt='img' />
+            <p>{t(e.text)}</p>
+          </div>
+        </div>
+      </div>
+    ))
+  }, [t])
+  return (
+    <div className={css.main}>
+      <div className={css.wrap}>
+        <div className={css.left}>
+          <h1>{t("slider.title")}</h1>
+          <p>{t("slider.text")}</p>
+          <button>{t("slider.btn")}</button>
+        </div>
+        <div className={css.slider}>
+          <Slider {...sliderSettings}>
+            {renderSlider}
+            {renderSlider}
+            {renderSlider}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NewsSlider
---
+++ src/constants/Slider.ts
@@ -0,0 +1,17 @@
+
+
+interface ISlider{
+    id:number,
+    img:string,
+    text:string
+}
+
+export const SliderData: ISlider[] = [
+    
+    
+    {
+        id:2,
+        img: '../../public/images/pic2.svg',
+        text:"slider.info2"
+    }
+]
