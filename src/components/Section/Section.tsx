10a5837b7623847e1366150d9321e6d3b1d800c6
kerexwe <adylovakeremet@gmail.com>
2023-08-17 16:06:38 +0600

    feat/fix

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,32 +1,84 @@
-import React from 'react'
-import scss from './Section.module.scss'
-import { SectionData } from '../../constants/SectionData'
-import Image from 'next/image'
-import { useMemo } from 'react'
+import React, { useState, useMemo } from "react";
+import scss from "./Section.module.scss";
+import { SectionData } from "../../constants/SectionData";
+import { SectionData2 } from "../../constants/SectionData2";
+
+import Image from "next/image";
 
 const Section = () => {
-    const getData = useMemo(() => SectionData.map(item => (
+  const [data, setData] = useState(SectionData);
+
+  const getData = useMemo(
+    () =>
+      data.map((item) => (
         <div className={scss.card} key={item.id}>
-            <div className={scss.card__item}>
-                <Image src={item.image} alt='Icon' />
-                <div className={scss.text}>
-                    <div className={scss.line}></div>
-                    <p>{item.title}</p>
-                </div>
+          <div className={scss.card__item}>
+            <Image src={item.image} alt="Icon" />
+            <div className={scss.text}>
+              <div className={scss.line}></div>
+              <p>{item.title}</p>
             </div>
+          </div>
         </div>
-    )), [])
+      )),
+    [data]
+  );
 
-    return (
-        <div className="container">
-            <div className={scss.wrapper}>
-                <h2>Выберите раздел</h2>
-                <div className={scss.cards}>
-                    {getData}
-                </div>
-            </div>
+  const handleHover = () => {
+    setData(isHovered ? SectionData : SectionData2);
+  };
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div className="container">
+      <div className={scss.wrapper}>
+        <h2>Выберите раздел</h2>
+        <div
+          className={scss.cards}
+          onMouseEnter={() => {
+            setIsHovered(true);
+            handleHover();
+          }}
+          onMouseLeave={() => {
+            setIsHovered(false);
+            handleHover();
+          }}
+        >
+          {getData}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Section
+export default Section;
+
+// const Section = () => {
+//   const getData = useMemo(
+//     () =>
+//       SectionData.map((item) => (
+//         <div className={scss.card} key={item.id}>
+//           <div className={scss.card__item}>
+//             <Image src={item.image} alt="Icon" />
+//             <div className={scss.text}>
+//               <div className={scss.line}></div>
+//               <p>{item.title}</p>
+//             </div>
+//           </div>
+//         </div>
+//       )),
+//     []
+//   );
+
+//   return (
+//     <div className="container">
+//       <div className={scss.wrapper}>
+//         <h2>Выберите раздел</h2>
+//         <div className={scss.cards}>{getData}</div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Section;
---
+++ src/constants/SectionData2.js
@@ -0,0 +1,45 @@
+import card1 from "../../public/images/SectionIcon/noteBlue.svg";
+import card2 from "../../public/images/SectionIcon/FrameBlue.svg";
+
+export const SectionData2 = [
+  {
+    id: 1,
+    image: card1,
+    title: "Вакансия по категориям",
+  },
+  {
+    id: 2,
+    image: card2,
+    title: "Вакансия по должностям",
+  },
+  {
+    id: 3,
+    image: card1,
+    title: "Работа для студентов",
+  },
+  {
+    id: 4,
+    image: card2,
+    title: "Удаленная работа",
+  },
+  {
+    id: 5,
+    image: card1,
+    title: "Вакансия по городам",
+  },
+  {
+    id: 6,
+    image: card2,
+    title: "Работа для пенсионеров",
+  },
+  {
+    id: 7,
+    image: card1,
+    title: "Вакансии по компаниям",
+  },
+  {
+    id: 8,
+    image: card2,
+    title: "Для людей с инвалидностью",
+  },
+];
