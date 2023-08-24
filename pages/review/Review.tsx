b7e2277cbf88c01cb9dd14a55915409347eace1a
Ammarz83~ <sapitovammar@gmail.com>
2023-08-24 18:36:11 +0600

    commit change slidersss

--- pages/review/Review.tsx
+++ pages/review/Review.tsx
@@ -1,22 +1,9 @@
 import React from 'react'
-import css from './Review.module.scss'
-import useReviewCard from '../review/Review'
-import { useTranslation } from 'react-i18next';
+import Reviews from '../../src/components/review/Review'
 
 function Review() {
-  const renderReviewCard = useReviewCard()
-  const { t } = useTranslation();
-
   return (
-    <div className={css.main}>
-      <div className='container'>
-        <h1 className={css.title}>{t("review.title")}</h1>
-        <div className={css.wrapper}>
-          {renderReviewCard}
-        </div>
-      </div>
-
-    </div>
+    <div></div>
   )
 }
 
